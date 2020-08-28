81f5ec3db6caeddcd8f2afdb573f418e40a1098c
Andrew Pham <andrewpham92@gmail.com>
2020-08-28 15:45:55 -0400

    added music files

--- template.py
+++ template.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 
+
+
 from pygame.locals import (
     RLEACCEL,
     K_UP,
@@ -14,7 +16,7 @@ from pygame.locals import (
 
 width = 500
 height = 500
-blue_color = (97, 159, 182)
+# blue_color = (97, 159, 182)
 # Define a player object by extending pygame.sprite.Sprite
 # The surface drawn on the screen is now an attribute of 'player
 class Player(pygame.sprite.Sprite):
@@ -48,7 +50,7 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy, self).__init__()
         self.surf = pygame.image.load('images/fly1.png').convert_alpha()
-        self.surf = pygame.transform.scale(self.surf, (30,30))
+        self.surf = pygame.transform.scale(self.surf, (25,25))
         self.surf.set_colorkey((255,255,255), RLEACCEL)
         self.rect = self.surf.get_rect(
             center=(
@@ -63,36 +65,41 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.move_ip(-self.speed, 0)    
         if self.rect.right < 0:
             self.kill()
-class Cloud(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Cloud, self) = pygame.image.load('image/sky.png').convert_alpha()
-        self.surf.set_colorkey((0,0,0), RLEACCEL)
-        # The starting position is randomly generated
-        self.rect = self.surf.get_rect(
-            center=(
-                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                random.randint(0, SCREEN_HEIGHT),
-            )
-        )
-    def update(self):
-        self.rect.move_ip(-5, 0)
-        if self.rect.right < 0:
-            self.kill()
+# class Cloud(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super(Cloud, self).__init__()
+#         self.surf = pygame.image.load('images/cloudy.png').convert_alpha()
+#         self.surf = pygame.transform.scale(self.surf, (30,30))
+#         self.surf.set_colorkey((0,0,0), RLEACCEL)
+#         # The starting position is randomly generated
+#         self.rect = self.surf.get_rect(
+#             center=(
+#                 random.randint(width + 20, width+ 100),
+#                 random.randint(0, height),
+#             )
+#         )
+#     def update(self):
+#         self.rect.move_ip(-5, 0)
+#         if self.rect.right < 0:
+#             self.kill()
 def main():
-
+    pygame.mixer.init()
     pygame.init()
     # Create the screen object
     # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('My Game')
     clock = pygame.time.Clock()
-
+    background_image = pygame.image.load('images/sky.png').convert_alpha()
+    #eartbeat by Snowflake (c) copyright 2016 Licensed under a Creative Commons Attribution Noncommercial  (3.0) license. http://dig.ccmixter.org/files/snowflake/53740 Ft: Scomber, George Ellinas, Vidian, Patronski#
+    pygame.mixer.music.load("sounds/snowflake_-_Heartbeat(online-audio-converter.com).wav")
+    pygame.mixer.music.play(loops=-1)
     # Create a custom event for adding new enemy
     ADDENEMY = pygame.USEREVENT + 1
     pygame.time.set_timer(ADDENEMY, 250)
     # Create custom events for adding a new enemy and a cloud
     ADDCLOUD = pygame.USEREVENT + 2
-    pygame.time.__set_timer(ADDCLOUD, 1000)
+    pygame.time.set_timer(ADDCLOUD, 1000)
 
     # Instantiate player. Right now, this is just a rectangle.
     player = Player()
@@ -123,24 +130,28 @@ def main():
                 enemies.add(new_enemy)
                 all_sprites.add(new_enemy) 
             # Add a new cloud
-            elif event.type == ADDCLOUD:
-                # Create the new cloud and add it to sprite groups
-                new_cloud = Cloud()
-                clouds.add(new_cloud)
-                all_sprites.add(new_cloud)    
+            # elif event.type == ADDCLOUD:
+            #     # Create the new cloud and add it to sprite groups
+            #     new_cloud = Cloud()
+            #     clouds.add(new_cloud)
+            #     all_sprites.add(new_cloud)    
 
         # Get the set of keys pressed and check for user input
         pressed_keys = pygame.key.get_pressed()
         player.update(pressed_keys)
 
+    
+
         # Update enemy position
         enemies.update()
+        clouds.update()
+        
 
 
         # Game logic
 
         # Draw background
-        screen.fill(blue_color)
+        screen.blit(background_image, [0, 0])
 
         # Draw all sprites
         for entity in all_sprites:
@@ -161,8 +172,8 @@ def main():
         # )
         # Draw surf at the new coordinates
         # screen.blit(surf, surf_center)
+        clock.tick(30)
         pygame.display.flip()
-        clock.tick(60)
 
     pygame.quit()
 
